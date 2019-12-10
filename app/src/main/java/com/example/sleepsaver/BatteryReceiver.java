c1f8ea0c8b7883461bd2a6c6ff6a52cf4ed99583
sousuke <sasuketchup1@gmail.com>
2019-12-10 13:33:45 +0900

    接続状態を判別して通知に表示(失敗)

--- app/src/main/java/com/example/sleepsaver/BatteryReceiver.java
+++ app/src/main/java/com/example/sleepsaver/BatteryReceiver.java
@@ -5,6 +5,7 @@ import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.BatteryManager;
 
 public class BatteryReceiver extends BroadcastReceiver {
 
@@ -12,11 +13,21 @@ public class BatteryReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 
+        int battery_charge = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
+        String charge_state = "";
+
+        if(battery_charge == 0){
+            charge_state = "起床時刻";
+        }else if(battery_charge == 1 || battery_charge == 2 || battery_charge == 4){
+            charge_state = "就寝時刻";
+        }
+
         Notification.Builder builder = new Notification.Builder(context);
         NotificationManager manager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
 
         builder.setSmallIcon(android.R.drawable.sym_def_app_icon);
         builder.setContentTitle("SleepSaver");
+        builder.setContentText(charge_state);
 
         manager.notify(1, builder.build());
     }
