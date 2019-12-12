1ef47432288a4bec5db20065dfc1c31f8f01c437
sousuke <sasuketchup1@gmail.com>
2019-12-12 13:29:28 +0900

    ポップアップするアクティビティで電源接続状態を取得

--- app/src/main/java/com/example/sleepsaver/PopUpActivity.java
+++ app/src/main/java/com/example/sleepsaver/PopUpActivity.java
@@ -1,18 +1,51 @@
 package com.example.sleepsaver;
 
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PopUpActivity extends AppCompatActivity {
+
+    TextView varTextGUorGTB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_popup);
 
+        // ロック画面上に表示
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
+
+        varTextGUorGTB = findViewById(R.id.textGUorGTB);
+
+        IntentFilter intentFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent battery_intent = this.registerReceiver(null, intentFilter);
+
+        int battery_charge = battery_intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
+        String charge_state = "";
+
+        if(battery_charge == 0){
+            charge_state = "起床時刻";
+        }else if(battery_charge == 1 || battery_charge == 2 || battery_charge == 4){
+            charge_state = "就寝時刻";
+        }
+
+        varTextGUorGTB.setText(charge_state);
+
+        findViewById(R.id.btnClose).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        finish();
+                    }
+                }
+        );
 
     }
 }
