1e7b613e65c21ee79344e925edc680788f37bf1a
sousuke <sasuketchup1@gmail.com>
2019-12-13 13:29:28 +0900

    ポップアップするアクティビティに時刻を表示

--- app/src/main/java/com/example/sleepsaver/PopUpActivity.java
+++ app/src/main/java/com/example/sleepsaver/PopUpActivity.java
@@ -10,9 +10,14 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Calendar;
+
 public class PopUpActivity extends AppCompatActivity {
 
     TextView varTextGUorGTB;
+    TextView varTextTime;
+
+    Calendar calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +28,29 @@ public class PopUpActivity extends AppCompatActivity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
 
         varTextGUorGTB = findViewById(R.id.textGUorGTB);
+        varTextTime = findViewById(R.id.textTime);
+
+        calendar = Calendar.getInstance();
+
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH) + 1;
+        final int date = calendar.get(Calendar.DATE);
+        final int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        final int minute = calendar.get(Calendar.MINUTE);
+
+        String hourSt;
+        String minuteSt;
+
+        if(hour<10){
+            hourSt = "0" + hour;
+        }else {
+            hourSt = String.valueOf(hour);
+        }
+        if(minute<10){
+            minuteSt = "0" + minute;
+        }else {
+            minuteSt = String.valueOf(minute);
+        }
 
         IntentFilter intentFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
         Intent battery_intent = this.registerReceiver(null, intentFilter);
@@ -36,7 +64,8 @@ public class PopUpActivity extends AppCompatActivity {
             charge_state = "就寝時刻";
         }
 
-        varTextGUorGTB.setText(charge_state);
+        varTextGUorGTB.setText(year + "年" + month + "月" + date + "日の" + charge_state);
+        varTextTime.setText(hourSt + ":" + minuteSt);
 
         findViewById(R.id.btnClose).setOnClickListener(
                 new View.OnClickListener() {
