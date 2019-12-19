3c51303933cbdbc9b55423c64918e599e6ab33e3
sousuke <sasuketchup1@gmail.com>
2019-12-19 13:26:44 +0900

    就寝時刻ボタン実装。当然落ちる。 ポップアップアクティビティを完成させよ！

--- app/src/main/java/com/example/sleepsaver/MainActivity.java
+++ app/src/main/java/com/example/sleepsaver/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     Calendar calendar;
     LinearLayout varDateLay;
     LinearLayout varGULay;
+    LinearLayout varGTBLay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,26 +38,33 @@ public class MainActivity extends AppCompatActivity {
 
         varDateLay = (LinearLayout) findViewById(R.id.DateLayout);
         varGULay = (LinearLayout) findViewById(R.id.GULayout);
+        varGTBLay = (LinearLayout) findViewById(R.id.GTBLayout);
 
         Cursor cursor = db.query("DateTable", new String[] {"id", "year", "month", "date"}, null, null, null, null, null);
         Cursor cursor1 = db.query("GetUpTable", new String[] {"id", "hour", "minute"}, null, null, null, null, null);
+        Cursor cursor2 = db.query("GoToBedTable", new String[] {"id", "hour", "minute"}, null, null, null, null, null);
 
         long idCount = DatabaseUtils.queryNumEntries(db, "DateTable");
 
         TextView[] textDate = new TextView[(int) idCount];
         TextView[] textGU = new TextView[(int) idCount];
+        TextView[] textGTB = new TextView[(int) idCount];
 
         // 記録を表示
         cursor.moveToFirst();
         cursor1.moveToFirst();
+        cursor2.moveToFirst();
         for(int i=0;i<idCount;i++){
             textDate[i] = new TextView(this);
             textGU[i] = new TextView(this);
-            int yearGU = cursor.getInt(1);
-            int monthGU = cursor.getInt(2);
-            int dateGU = cursor.getInt(3);
+            textGTB[i] = new TextView(this);
+            int year = cursor.getInt(1);
+            int month = cursor.getInt(2);
+            int date = cursor.getInt(3);
             int hourGU = cursor1.getInt(1);
             int minuteGU = cursor1.getInt(2);
+            int hourGTB = cursor2.getInt(1);
+            int minuteGTB = cursor2.getInt(2);
 
             String hourGUSt;
             String minuteGUSt;
@@ -71,15 +79,32 @@ public class MainActivity extends AppCompatActivity {
                 minuteGUSt = String.valueOf(minuteGU);
             }
 
-            textDate[i].setText(yearGU + "年" + monthGU + "月" + dateGU + "日");
+            String hourGTBSt;
+            String minuteGTBSt;
+            if(hourGTB<10){
+                hourGTBSt = "0" + hourGTB;
+            }else {
+                hourGTBSt = String.valueOf(hourGTB);
+            }
+            if(minuteGTB<10){
+                minuteGTBSt = "0" + minuteGTB;
+            }else {
+                minuteGTBSt = String.valueOf(minuteGTB);
+            }
+
+            textDate[i].setText(year + "年" + month + "月" + date + "日");
             textGU[i].setText(hourGUSt + ":" + minuteGUSt);
+            textGTB[i].setText(hourGTBSt + ":" + minuteGTBSt);
             cursor.moveToNext();
             cursor1.moveToNext();
+            cursor2.moveToNext();
             varDateLay.addView(textDate[i], 0);
             varGULay.addView(textGU[i], 0);
+            varGTBLay.addView(textGTB[i], 0);
         }
         cursor.close();
         cursor1.close();
+        cursor2.close();
 
         final ContentValues contentValues = new ContentValues();
         final ContentValues contentValues1 = new ContentValues();
@@ -154,18 +179,72 @@ public class MainActivity extends AppCompatActivity {
                 }
         );
 
+        // 就寝時刻ボタンの処理
         findViewById(R.id.GTBbtn).setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
 
-                        Intent intent = new Intent(MainActivity.this, PopUpActivity.class);
-                        PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, 0, intent, 0);
-                        try {
-                            pendingIntent.send();
-                        } catch (PendingIntent.CanceledException e) {
-                            e.printStackTrace();
+                        final long idNumber = DatabaseUtils.queryNumEntries(db, "DateTable");
+
+                        calendar = Calendar.getInstance();
+
+                        final int year = calendar.get(Calendar.YEAR);
+                        final int month = calendar.get(Calendar.MONTH) + 1;
+                        final int date = calendar.get(Calendar.DATE);
+                        final int hour = calendar.get(Calendar.HOUR_OF_DAY);
+                        final int minute = calendar.get(Calendar.MINUTE);
+
+                        String hourSt;
+                        String minuteSt;
+                        if(hour<10){
+                            hourSt = "0" + hour;
+                        }else {
+                            hourSt = String.valueOf(hour);
                         }
+                        if(minute<10){
+                            minuteSt = "0" + minute;
+                        }else {
+                            minuteSt = String.valueOf(minute);
+                        }
+
+                        TextView tvTime = new TextView(getApplicationContext());
+                        tvTime.setText(hourSt + ":" + minuteSt);
+                        tvTime.setTextColor(Color.BLACK);
+                        tvTime.setTextSize(30);
+                        tvTime.setGravity(Gravity.CENTER_HORIZONTAL);
+
+                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                        builder.setTitle(year + "年" + month + "月" + date + "日の就寝時刻");
+                        builder.setView(tvTime);
+                        builder.setPositiveButton(
+                                "記録",
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        contentValues.put("id", idNumber);
+                                        contentValues.put("year", year);
+                                        contentValues.put("month", month);
+                                        contentValues.put("date", date);
+
+                                        contentValues1.put("id", idNumber);
+                                        contentValues1.put("hour", hour);
+                                        contentValues1.put("minute", minute);
+
+                                        db.insert("DateTable", null, contentValues);
+                                        db.insert("GoToBedTable", null, contentValues1);
+
+
+                                        finish();
+                                        overridePendingTransition(0, 0);
+                                        startActivity(getIntent());
+                                        overridePendingTransition(0, 0);
+
+                                        dialog.dismiss();
+                                    }
+                                }
+                        );
+                        builder.show();
                     }
                 }
         );
